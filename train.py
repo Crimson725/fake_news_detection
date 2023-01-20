a4fd5f1c170b36c6460df35a784cccb830fbaed4
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2023-01-20 17:06:57 +0800

    add sent mode

--- train.py
+++ train.py
@@ -11,8 +11,9 @@ from models.layers import customBERT
 from utils.common_util import (
     save_checkpoint,
     save_metrics,
+    SentDataset,
 )
-from utils.common_util import CustomDataset, tokenizer, Dataloader_train
+from utils.common_util import DocDataset, tokenizer, Dataloader_train
 from utils.logger import Logger
 import time
 import datetime
@@ -45,7 +46,7 @@ class Trainer:
 
         self.early_stop_patience = self.params.early_stop_patience
 
-    def get_loader(self):
+    def get_doc_loader(self):
         def seed_worker(worker_id):
             worker_seed = self.params.seed
             np.random.seed(worker_seed)
@@ -76,8 +77,8 @@ class Trainer:
             test_dataset = df.drop(train_dataset.index).reset_index(drop=True)
 
             # get the train set and test set
-            train_set = CustomDataset(train_dataset, tokenizer, self.params.max_len)
-            test_set = CustomDataset(test_dataset, tokenizer, self.params.max_len)
+            train_set = DocDataset(train_dataset, tokenizer, self.params.max_len)
+            test_set = DocDataset(test_dataset, tokenizer, self.params.max_len)
             training_loader = DataLoader(train_set, **train_loader_params)
             testing_loader = DataLoader(test_set, **test_loader_params)
         else:
@@ -87,8 +88,56 @@ class Trainer:
             test_dataset = pd.read_csv(
                 os.path.join(CONFIG.DATA_PATH, self.params.valid_dataset)
             )
-            train_set = CustomDataset(train_dataset, tokenizer, self.params.max_len)
-            test_set = CustomDataset(test_dataset, tokenizer, self.params.max_len)
+            train_set = DocDataset(train_dataset, tokenizer, self.params.max_len)
+            test_set = DocDataset(test_dataset, tokenizer, self.params.max_len)
+            training_loader = DataLoader(train_set, **train_loader_params)
+            testing_loader = DataLoader(test_set, **test_loader_params)
+        return training_loader, testing_loader
+
+    def get_sent_loader(self):
+        def seed_worker(worker_id):
+            worker_seed = self.params.seed
+            np.random.seed(worker_seed)
+            random.seed(worker_seed)
+
+        g = torch.Generator()
+        g.manual_seed(self.params.seed)
+
+        train_loader_params = {
+            "batch_size": self.params.train_batch,
+            "shuffle": True,
+            "worker_init_fn": seed_worker,
+            "generator": g,
+        }
+        test_loader_params = {
+            "batch_size": self.params.test_batch,
+            "shuffle": True,
+            "worker_init_fn": seed_worker,
+            "generator": g,
+        }
+        # dataset settings
+        if self.params.valid_enable is False:
+            train_size = self.params.frac
+            df = pd.read_csv(os.path.join(CONFIG.DATA_PATH, self.params.dataset))
+            train_dataset = df.sample(frac=train_size, random_state=200).reset_index(
+                drop=True
+            )
+            test_dataset = df.drop(train_dataset.index).reset_index(drop=True)
+
+            # get the train set and test set
+            train_set = SentDataset(train_dataset, tokenizer)
+            test_set = SentDataset(test_dataset, tokenizer)
+            training_loader = DataLoader(train_set, **train_loader_params)
+            testing_loader = DataLoader(test_set, **test_loader_params)
+        else:
+            train_dataset = pd.read_csv(
+                os.path.join(CONFIG.DATA_PATH, self.params.dataset)
+            )
+            test_dataset = pd.read_csv(
+                os.path.join(CONFIG.DATA_PATH, self.params.valid_dataset)
+            )
+            train_set = SentDataset(train_dataset, tokenizer)
+            test_set = SentDataset(test_dataset, tokenizer)
             training_loader = DataLoader(train_set, **train_loader_params)
             testing_loader = DataLoader(test_set, **test_loader_params)
         return training_loader, testing_loader
@@ -101,7 +150,7 @@ class Trainer:
         os.mkdir(tf_path)
         return file_path, tf_path
 
-    def train_customBERT(self, best_valid_loss=float("Inf")):
+    def train_doc(self, best_valid_loss=float("Inf")):
 
         loader = Dataloader_train(self.params)
         # get the dataloader
@@ -241,6 +290,193 @@ class Trainer:
                                 f"Early stopping in step:{global_step}/{epochs * len(training_loader)}"
                             )
                             break
+
+        if self.params.valid_enable:
+            eval_model = self.model
+            evaluator = Evaluator(
+                eval_model,
+                testing_loader,
+                device=self.device,
+                params=None,
+                model_path=model_path,
+            )
+            logger.log("--------------------Evaluation--------------------")
+            evaluator.validation(logger)
+
+        save_metrics(
+            metrics_path,
+            train_loss_list,
+            valid_loss_list,
+            global_step_list,
+        )
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        logger.log(
+            f"Model: {str(model.__class__.__name__)}, Best valid loss: {best_valid_loss}, Elapsed time: {elapsed_time}"
+        )
+        print("Finished Training!")
+
+    def train_sent(self, best_valid_loss=float("Inf")):
+
+        loader = Dataloader_train(self.params)
+        # get the dataloader
+        training_loader, testing_loader = loader.get_loader()
+
+        # get the model
+        model = self.model
+        model_name = self.model_name
+        # make the path to save the log and models
+        file_path, tf_path = self.get_path(model_name)
+        model_path = file_path + "/" + "model.pt"
+        best_metrics_path = file_path + "/" + "best_metrics.pt"
+        metrics_path = file_path + "/" + "metrics.pt"
+        train_args_path = file_path + "/" + "train_args.pkl"
+        # save the args
+        with open(train_args_path, "wb") as f:
+            pickle.dump(self.params, f)
+        # train/valid loss
+        running_loss = 0
+        valid_running_loss = 0
+        global_step = 0
+        train_loss_list = []
+        valid_loss_list = []
+        global_step_list = []
+        # early stop counter
+        early_stop_counter = 0
+
+        # eval setting
+        eval_every = len(training_loader) // self.params.eval_every
+        # get the logger
+        logger = Logger("Training", file_path)
+        tflogger = SummaryWriter(tf_path)
+
+        # training
+        model.train()
+        start_time = time.time()
+        logger.log("Start training...")
+        logger.log("Training size: {}".format(len(training_loader.dataset)))
+        logger.log("Test size: {}".format(len(testing_loader.dataset)))
+
+        logger.log("--------------------Args--------------------")
+        # print and log args
+        print("--------------------Args--------------------")
+        if self.params.log_args:
+            for k, v in vars(self.params).items():
+                logger.log(f"{k} = {v}")
+                print(f"{k} = {v}")
+        #  training args
+        epochs = self.epochs
+        loss_fn = self.loss_fn
+        optimizer = self.optimizer
+        verbose = self.params.verbose
+        print_logs = self.params.print_logs
+        logger.log("--------------------Loss--------------------")
+        for epoch in range(epochs):
+            for _, data in enumerate(training_loader):
+                for i in range(data["ids"].size(1)):
+                    # get data
+                    ids = data["ids"][:, i].unsqueeze(1)
+                    mask = data["mask"][:, i].unsqueeze(1)
+                    token_type_ids = data["token_type_ids"][:, i].unsqueeze(1)
+                    targets = data["targets"][i]
+                    # to device
+                    ids = ids.to(self.device, dtype=torch.long)
+                    mask = mask.to(self.device, dtype=torch.long)
+                    token_type_ids = token_type_ids.to(self.device, dtype=torch.long)
+                    targets = targets.to(self.device, dtype=torch.float)
+
+                    optimizer.zero_grad()
+                    output = model(ids, mask, token_type_ids)
+
+                    loss = loss_fn(output, targets)
+                    loss.backward()
+                    optimizer.step()
+
+                    running_loss += loss.item()
+                    global_step += 1
+                    if global_step % eval_every == 0:
+                        model.eval()
+                        with torch.no_grad():
+                            for _, data in enumerate(testing_loader):
+                                for i in range(data["ids"].size(1)):
+                                    # get data
+                                    ids = data["ids"][:, i].unsqueeze(1)
+                                    mask = data["mask"][:, i].unsqueeze(1)
+                                    token_type_ids = data["token_type_ids"][
+                                        :, i
+                                    ].unsqueeze(1)
+                                    targets = data["targets"][i]
+                                    # to device
+                                    ids = ids.to(self.device, dtype=torch.long)
+                                    mask = mask.to(self.device, dtype=torch.long)
+                                    token_type_ids = token_type_ids.to(
+                                        self.device, dtype=torch.long
+                                    )
+                                    targets = targets.to(self.device, dtype=torch.float)
+
+                                    output = model(ids, mask, token_type_ids)
+                                    loss = loss_fn(output, targets)
+                                    valid_running_loss += loss.item()
+
+                            average_train_loss = running_loss / eval_every
+                            average_valid_loss = valid_running_loss / len(
+                                testing_loader
+                            )
+                            train_loss_list.append(average_train_loss)
+                            valid_loss_list.append(average_valid_loss)
+                            global_step_list.append(global_step)
+                            if tflogger is not None:
+                                tflogger.add_scalar(
+                                    "Training loss", average_train_loss, global_step
+                                )
+                                tflogger.add_scalar(
+                                    "Validation loss", average_valid_loss, global_step
+                                )
+                                for name, param in model.named_parameters():
+                                    tflogger.add_histogram(
+                                        name,
+                                        param.clone().cpu().data.numpy(),
+                                        global_step,
+                                    )
+
+                            running_loss = 0
+                            valid_running_loss = 0
+                            model.train()
+                            msg = "Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.4f}".format(
+                                epoch + 1,
+                                epochs,
+                                global_step,
+                                epochs * len(training_loader),
+                                average_train_loss,
+                                average_valid_loss,
+                            )
+                            if verbose:
+                                if print_logs:
+                                    # print the training msg
+                                    print(msg)
+                                logger.log(msg)
+                            if best_valid_loss > average_valid_loss:
+                                best_valid_loss = average_valid_loss
+                                early_stop_counter = 0
+                                save_checkpoint(model_path, model, best_valid_loss)
+                                save_metrics(
+                                    best_metrics_path,
+                                    train_loss_list,
+                                    valid_loss_list,
+                                    global_step_list,
+                                )
+                                model.config.to_json_file(
+                                    file_path + "/" + "config.json"
+                                )
+                            else:
+                                early_stop_counter += 1
+                            if early_stop_counter >= self.early_stop_patience:
+                                print("Early stopping")
+                                logger.log(
+                                    f"Early stopping in step:{global_step}/{epochs * len(training_loader)}"
+                                )
+                                break
 
         if self.params.valid_enable:
             eval_model = self.model
--- utils/kg_util.py
+++ utils/kg_util.py
@@ -1,4 +1,16 @@
 import requests
+import re
+import hashlib
+
+import spacy
+from spacy import Language
+from typing import List
+from spacy.tokens import Doc, Span
+from transformers import pipeline
+import fastcoref
+from fastcoref import spacy_component
+
+DEVICE = 0
 
 
 def call_wiki_api(item):
@@ -64,3 +76,130 @@ def extract_triplets(text):
         )
 
     return triplets
+
+
+@Language.factory(
+    "rebel",
+    requires=["doc.sents"],
+    assigns=["doc._.rel"],
+    default_config={
+        "model_name": "Babelscape/rebel-large",
+        "device": DEVICE,
+    },
+)
+class RebelComponent:
+    def __init__(
+        self,
+        nlp,
+        name,
+        model_name: str,
+        device: int,
+    ):
+        assert model_name is not None, ""
+        self.triplet_extractor = pipeline(
+            "text2text-generation",
+            model=model_name,
+            tokenizer=model_name,
+            device=device,
+        )
+        self.entity_mapping = {}
+        # Register custom extension on the Doc
+        if not Doc.has_extension("rel"):
+            Doc.set_extension("rel", default={})
+
+    def get_wiki_id(self, item: str):
+        mapping = self.entity_mapping.get(item)
+        if mapping:
+            return mapping
+        else:
+            res = call_wiki_api(item)
+            self.entity_mapping[item] = res
+            return res
+
+    def _generate_triplets(self, sent: Span) -> List[dict]:
+        output_ids = self.triplet_extractor(
+            sent.text, return_tensors=True, return_text=False
+        )[0]["generated_token_ids"]["output_ids"]
+        extracted_text = self.triplet_extractor.tokenizer.batch_decode(output_ids[0])
+        extracted_triplets = extract_triplets(extracted_text[0])
+        return extracted_triplets
+
+    def set_annotations(self, doc: Doc, triplets: List[dict]):
+        for triplet in triplets:
+
+            # Remove self-loops (relationships that start and end at the entity)
+            if triplet["head"] == triplet["tail"]:
+                continue
+
+            # Use regex to search for entities
+            head_span = re.search(triplet["head"], doc.text)
+            tail_span = re.search(triplet["tail"], doc.text)
+
+            # Skip the relation if both head and tail entities are not present in the text
+            # Sometimes the Rebel model hallucinates some entities
+            if not head_span or not tail_span:
+                continue
+
+            index = hashlib.sha1(
+                "".join([triplet["head"], triplet["tail"], triplet["type"]]).encode(
+                    "utf-8"
+                )
+            ).hexdigest()
+            if index not in doc._.rel:
+                # Get wiki ids and store results
+                doc._.rel[index] = {
+                    "relation": triplet["type"],
+                    "head_span": {
+                        "text": triplet["head"],
+                        "id": self.get_wiki_id(triplet["head"]),
+                    },
+                    "tail_span": {
+                        "text": triplet["tail"],
+                        "id": self.get_wiki_id(triplet["tail"]),
+                    },
+                }
+
+    def __call__(self, doc: Doc) -> Doc:
+        for sent in doc.sents:
+            sentence_triplets = self._generate_triplets(sent)
+            self.set_annotations(doc, sentence_triplets)
+        return doc
+
+
+# coref
+
+
+if __name__ == "__main__":
+    # get coref pipeline
+    nlp = spacy.load(
+        "en_core_web_sm", exclude=["parser", "lemmatizer", "ner", "textcat"]
+    )
+    nlp.add_pipe(
+        "fastcoref",
+        config={
+            "model_architecture": "LingMessCoref",
+            "model_path": "biu-nlp/lingmess-coref",
+            "device": "cuda:0",
+        },
+    )
+    # get rel_ext pipeline
+    rel_ext = spacy.load(
+        "en_core_web_sm", disable=["ner", "lemmatizer", "attribute_rules", "tagger"]
+    )
+    rel_ext.add_pipe(
+        "rebel",
+        config={
+            "device": DEVICE,  # Number of the GPU, -1 if want to use CPU
+            "model_name": "Babelscape/rebel-large",
+        },
+    )  # Model used, will default to 'Babelscape/rebel-large'
+
+    text = "Alice goes down the rabbit hole. Where she would discover a new reality beyond her expectations."
+
+    doc = nlp(text, component_cfg={"fastcoref": {"resolve_text": True}})
+    # get coref results
+    coref_text = doc._.coref_resolved
+
+    # get the rel_ext results
+    result = rel_ext(coref_text)
+    print(result._.rel)
