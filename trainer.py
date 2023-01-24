3dc966d0c53c408adc23ae18da0549c4f6369c34
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2023-01-24 17:38:37 +0800

    removed ddp

--- trainer.py
+++ trainer.py
@@ -3,9 +3,12 @@ import os
 from utils.common_util import seed_everything
 from utils.common_util import get_train_parser
 import platform
+import torch.distributed as dist
+
 
 # set the environment variable
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+
 
 
 def train(params):
