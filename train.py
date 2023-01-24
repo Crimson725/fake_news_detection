fa400c0a36779c38bbcda830187a6bd15c5f91e9
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2023-01-24 16:05:29 +0800

    Update train.py

--- train.py
+++ train.py
@@ -24,10 +24,11 @@ class Trainer:
         self.params = params
         if ',' in self.params.cuda:
             # Use DataParallel for multiple GPUs
-            device = 'cuda'
+            self.device = 'cuda'
             os.environ["CUDA_VISIBLE_DEVICES"] = self.params.cuda
         else:
-            self.device = torch.device(torch.device("cuda:{}".format(self.params.cuda)))
+            self.device = f'cuda:{self.params.cuda}'
+            torch.cuda.set_device(int(self.params.cuda))
         # define the customBERT
         # load the default config
         if self.params.bert_type== "bert-base-uncased":
