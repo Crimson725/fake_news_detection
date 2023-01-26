19b2f599ba308b1c944e71b97465812265753ba9
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2023-01-26 10:44:16 +0800

    add ddp

--- trainer.py
+++ trainer.py
@@ -1,23 +1,35 @@
+from transformers import BertConfig
+import torch
+import CONFIG
+from models.layers import customBERT
 from train import Trainer
 import os
-from utils.common_util import seed_everything
+from utils.common_util import seed_init
 from utils.common_util import get_train_parser
 import platform
-import torch.distributed as dist
 
 
-# set the environment variable
+# for reproducibility
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
 
+def main(params):
+    # init the model
+    if params.bert_type == "bert-base-uncased":
+        config = BertConfig(label2id=CONFIG.LABEL2ID, id2label=CONFIG.ID2LABEL)
+    if params.bert_type == "bert-large-uncased":
+        config = BertConfig.from_json_file(os.path.join(CONFIG.BERT_LARGE_PATH, "config.json"))
+        config.label2id = CONFIG.LABEL2ID
+        config.id2label = CONFIG.ID2LABEL
+    device = torch.device(torch.device("cuda:{}".format(params.cuda)))
+    model = customBERT(config, params=params).to(device)
 
-def train(params):
-    trainer = Trainer(params)
-    trainer.train_doc()
+    trainer = Trainer(params, model, device)
+    trainer.train()
 
 
 if __name__ == "__main__":
     params = get_train_parser()
     if platform.system() == "Linux":
-        seed_everything(params.seed)
-    train(params)
+        seed_init(params.seed)
+    main(params)
--- utils/common_util.py
+++ utils/common_util.py
@@ -3,22 +3,32 @@ import random, os
 import numpy as np
 import pandas as pd
 import torch
+from torch.utils.data.distributed import DistributedSampler
+from torch.backends import cudnn
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer
 import CONFIG
 
 
-
-
-
-def seed_everything(seed):
+def seed_init(seed):
     random.seed(seed)
     torch.manual_seed(seed)
     torch.use_deterministic_algorithms(True)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        # torch.backends.cudnn.cudnn.benchmark = False
-        torch.backends.cudnn.deterministic = True
+        cudnn.deterministic = True
+
+
+def ddp_seed_init(seed, cuda_deterministic=True):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda_deterministic:  # slower, more reproducible
+        cudnn.deterministic = True
+        cudnn.benchmark = False
+    else:  # faster, less reproducible
+        cudnn.deterministic = False
+        cudnn.benchmark = True
 
 
 def save_checkpoint(path, model, valid_loss):
@@ -28,6 +38,12 @@ def save_checkpoint(path, model, valid_loss):
     torch.save(state_dict, path)
     print("model saved to ==>{}".format(path))
 
+def save_ddp_checkpoint(path, model, valid_loss):
+    if path == None:
+        return
+    state_dict = {"model_state_dict": model.module.state_dict(), "valid_loss": valid_loss}
+    torch.save(state_dict, path)
+    print("model saved to ==>{}".format(path))
 
 def load_checkpoint(path, model):
     if path == None:
@@ -62,26 +78,25 @@ def load_metrics(path):
     )
 
 
-
-
 class DocDataset(Dataset):
-    def __init__(self, dataframe,params):
+    def __init__(self, dataframe, params):
         self.data = dataframe
         self.text = dataframe.text
         self.targets = dataframe.label
-        self.params=params
+        self.params = params
         self.max_len = self.params.max_len
         # get tokenizer
-        if self.params.bert_type=="bert-base-uncased":
+        if self.params.bert_type == "bert-base-uncased":
             self.tokenizer = BertTokenizer.from_pretrained(CONFIG.BERT_BASE_PATH)
-        if self.params.bert_type=="bert-large-uncased":
+        if self.params.bert_type == "bert-large-uncased":
             self.tokenizer = BertTokenizer.from_pretrained(CONFIG.BERT_LARGE_PATH)
-        # bert tokenizer parameters
-        # MAX_SEQ_LEN = 128
-        # PAD_INDEX = self.tokenizer.convert_tokens_to_ids(self.tokenizer.pad_token)
-        # UNK_INDEX = self.tokenizer.convert_tokens_to_ids(self.tokenizer.unk_token)
 
-        # the entity encoder
+    # bert tokenizer parameters
+    # MAX_SEQ_LEN = 128
+    # PAD_INDEX = self.tokenizer.convert_tokens_to_ids(self.tokenizer.pad_token)
+    # UNK_INDEX = self.tokenizer.convert_tokens_to_ids(self.tokenizer.unk_token)
+
+    # the entity encoder
 
     def __len__(self):
         return len(self.text)
@@ -114,7 +129,7 @@ class DocDataset(Dataset):
         }
 
 
-class Docloader_train:
+class loader_train:
     def __init__(self, params):
         self.params = params
 
@@ -167,7 +182,70 @@ class Docloader_train:
         return training_loader, testing_loader
 
 
-class Docloader_eval:
+class DDP_loader_train:
+    def __init__(self, params):
+        self.params = params
+
+    def get_loader(self):
+        def seed_worker(worker_id):
+            worker_seed = self.params.seed
+            np.random.seed(worker_seed)
+            random.seed(worker_seed)
+
+        g = torch.Generator()
+        g.manual_seed(self.params.seed)
+
+        train_loader_params = {
+            "batch_size": self.params.train_batch // 2,
+            "worker_init_fn": seed_worker,
+            "generator": g,
+        }
+        test_loader_params = {
+            "batch_size": self.params.test_batch // 2,
+            "worker_init_fn": seed_worker,
+            "generator": g,
+        }
+        # dataset settings
+        if self.params.valid_enable is False:
+            train_size = self.params.frac
+            df = pd.read_csv(os.path.join(CONFIG.DATA_PATH, self.params.dataset))
+            train_dataset = df.sample(frac=train_size, random_state=200).reset_index(
+                drop=True
+            )
+            test_dataset = df.drop(train_dataset.index).reset_index(drop=True)
+
+            # get the train set and test set
+            train_set = DocDataset(train_dataset, self.params)
+            test_set = DocDataset(test_dataset, self.params)
+
+            # get the sampler
+            train_sampler = DistributedSampler(train_set)
+            test_sampler = DistributedSampler(test_set)
+
+            # get the dataloader
+            training_loader = DataLoader(train_set, **train_loader_params, sampler=train_sampler)
+            testing_loader = DataLoader(test_set, **test_loader_params, sampler=test_sampler)
+        else:
+            train_dataset = pd.read_csv(
+                os.path.join(CONFIG.DATA_PATH, self.params.dataset)
+            )
+            test_dataset = pd.read_csv(
+                os.path.join(CONFIG.DATA_PATH, self.params.valid_dataset)
+            )
+            # get the dataset
+            train_set = DocDataset(train_dataset, self.params)
+            test_set = DocDataset(test_dataset, self.params)
+
+            # get the sampler
+            train_sampler = DistributedSampler(train_set)
+            test_sampler = DistributedSampler(test_set)
+            training_loader = DataLoader(train_set, **train_loader_params,sampler=train_sampler)
+            testing_loader = DataLoader(test_set, **test_loader_params,sampler=test_sampler)
+        return training_loader, testing_loader,train_sampler,test_sampler
+
+
+
+class loader_eval:
     def __init__(self, params, train_args):
         self.params = params
         self.train_args = train_args
@@ -196,8 +274,7 @@ class Docloader_eval:
 
 def get_train_parser():
     argparser = argparse.ArgumentParser(
-        description="Arg parser for fake news detection. Implemented model: BERT, TextCNN",
-        epilog='For example: python trainer.py --seed 42 --cuda 5 --dataset "LUN/lun_train_comparenet.csv" --valid_dataset "LUN/lun_test_comparenet.csv" --weight_decay 0.2 --epochs 3 --lstm --num_layers 2 --multihead_attention --num_heads 12',
+        description="Arg parser for fake news detection. Implemented model: BERT",
     )
 
     argparser.add_argument("--seed", type=int, default=42, help="seed")
@@ -238,7 +315,7 @@ def get_train_parser():
         "--max_len", type=int, default=512, help="max length to padding (For doc mode)"
     )
     argparser.add_argument("--epochs", type=int, default=1, help="epoch of training ")
-    argparser.add_argument("--bert_type",type=str,default='bert-base-uncased',help="type of bert model")
+    argparser.add_argument("--bert_type", type=str, default='bert-base-uncased', help="type of bert model")
     argparser.add_argument(
         "--lstm",
         type=bool,
@@ -302,5 +379,98 @@ def get_eval_parser():
         "--eval_dataset", type=str, default=None, help="path to the evaluation dataset"
     )
 
+    args = argparser.parse_args()
+    return args
+
+
+def get_train_parser_ddp():
+    argparser = argparse.ArgumentParser(
+        description="Arg parser for fake news detection DDP training. Implemented model: BERT",
+    )
+
+    argparser.add_argument("--seed", type=int, default=42, help="seed")
+
+    argparser.add_argument(
+        "--dataset", type=str, default="real_and_fake/train.csv", help="dataset"
+    )
+    argparser.add_argument(
+        "--valid_enable",
+        action="store_true",
+        default=True,
+        help="validation using another dataset",
+    )
+    valid_enable_group = argparser.add_mutually_exclusive_group(required=True)
+    valid_enable_group.add_argument(
+        "--frac",
+        type=float,
+        default=None,
+        help="the fraction of the dataset to use for validation (only when valid_enable is False)",
+    )
+    valid_enable_group.add_argument(
+        "--valid_dataset",
+        type=str,
+        default=None,
+        help="the path to the validation dataset (only when valid_enable is True)",
+    )
+    argparser.add_argument("--lr", type=float, default=1e-5, help="learning rate")
+    argparser.add_argument(
+        "--train_batch", type=int, default=8, help="training set batch size"
+    )
+    argparser.add_argument(
+        "--test_batch", type=int, default=8, help="validation set batch size"
+    )
+    argparser.add_argument(
+        "--eval_every", type=int, default=5, help="evaluate every n step"
+    )
+    argparser.add_argument(
+        "--max_len", type=int, default=512, help="max length to padding (For doc mode)"
+    )
+    argparser.add_argument("--epochs", type=int, default=1, help="epoch of training ")
+    argparser.add_argument("--bert_type", type=str, default='bert-base-uncased', help="type of bert model")
+    argparser.add_argument(
+        "--lstm",
+        type=bool,
+        default=False,
+        help="use lstm",
+    )
+    argparser.add_argument(
+        "--multihead_attention",
+        type=bool,
+        default=False,
+        help="use multihead attention",
+    )
+
+    argparser.add_argument(
+        "--dropout", type=float, default=0.5, help="dropout rate of the model"
+    )
+    argparser.add_argument(
+        "--early_stop_patience", type=int, default=5, help="early stop patience"
+    )
+
+    argparser.add_argument(
+        "--weight_decay", type=float, default=0.0, help="weight decay of adam"
+    )
+    argparser.add_argument(
+        "--model_name", type=str, default="customBERT", help="name of the model"
+    )
+
+    argparser.add_argument(
+        "--log_args",
+        type=bool,
+        default=True,
+        help="log the args of the training process",
+    )
+    argparser.add_argument(
+        "--verbose",
+        type=bool,
+        default=True,
+        help="log verbose (loss) info of the training process",
+    )
+    argparser.add_argument(
+        "--print_logs",
+        type=bool,
+        default=False,
+        help="print the verbose info of the training process",
+    )
     args = argparser.parse_args()
     return args
